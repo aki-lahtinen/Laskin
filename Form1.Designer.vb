5f4a9c5cc47129594a592d1838e2991b48bb3633
Aki Lahtinen <aki.lahtinen@live.fi>
2025-12-25 16:50:01 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,355 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        TextBox1 = New TextBox()
+        Button18 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.Control
+        Button1.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(32, 295)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(63, 54)
+        Button1.TabIndex = 0
+        Button1.TabStop = False
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.Control
+        Button2.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(101, 295)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(63, 54)
+        Button2.TabIndex = 1
+        Button2.TabStop = False
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.Control
+        Button3.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(170, 295)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(63, 54)
+        Button3.TabIndex = 2
+        Button3.TabStop = False
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = SystemColors.Control
+        Button4.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button4.Location = New Point(32, 235)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(63, 54)
+        Button4.TabIndex = 3
+        Button4.TabStop = False
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = SystemColors.Control
+        Button5.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button5.Location = New Point(101, 235)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(63, 54)
+        Button5.TabIndex = 4
+        Button5.TabStop = False
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = SystemColors.Control
+        Button6.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button6.Location = New Point(170, 235)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(63, 54)
+        Button6.TabIndex = 5
+        Button6.TabStop = False
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = SystemColors.Control
+        Button7.FlatAppearance.BorderColor = SystemColors.ButtonHighlight
+        Button7.FlatAppearance.BorderSize = 2
+        Button7.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button7.Location = New Point(32, 175)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(63, 54)
+        Button7.TabIndex = 6
+        Button7.TabStop = False
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = SystemColors.Control
+        Button8.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button8.Location = New Point(101, 175)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(63, 54)
+        Button8.TabIndex = 7
+        Button8.TabStop = False
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = SystemColors.Control
+        Button9.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button9.Location = New Point(170, 175)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(63, 54)
+        Button9.TabIndex = 8
+        Button9.TabStop = False
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = SystemColors.Control
+        Button10.FlatAppearance.BorderSize = 2
+        Button10.Font = New Font("Sans Serif Collection", 14.25F)
+        Button10.Location = New Point(32, 355)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(63, 54)
+        Button10.TabIndex = 9
+        Button10.TabStop = False
+        Button10.Text = "0"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = SystemColors.Control
+        Button11.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button11.Location = New Point(101, 355)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(63, 54)
+        Button11.TabIndex = 10
+        Button11.TabStop = False
+        Button11.Text = ","
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = SystemColors.GradientInactiveCaption
+        Button12.FlatAppearance.BorderColor = SystemColors.Control
+        Button12.FlatAppearance.BorderSize = 2
+        Button12.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button12.Location = New Point(170, 355)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(132, 54)
+        Button12.TabIndex = 11
+        Button12.TabStop = False
+        Button12.Text = "="
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = SystemColors.GradientInactiveCaption
+        Button13.FlatAppearance.BorderColor = SystemColors.Control
+        Button13.FlatAppearance.BorderSize = 2
+        Button13.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button13.Location = New Point(239, 295)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(63, 54)
+        Button13.TabIndex = 12
+        Button13.TabStop = False
+        Button13.Text = "+"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = SystemColors.GradientInactiveCaption
+        Button14.FlatAppearance.BorderColor = SystemColors.Control
+        Button14.FlatAppearance.BorderSize = 2
+        Button14.Font = New Font("Sans Serif Collection", 14.25F)
+        Button14.Location = New Point(239, 235)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(63, 54)
+        Button14.TabIndex = 13
+        Button14.TabStop = False
+        Button14.Text = "-"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = SystemColors.GradientInactiveCaption
+        Button15.FlatAppearance.BorderColor = SystemColors.Control
+        Button15.FlatAppearance.BorderSize = 2
+        Button15.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button15.Location = New Point(239, 175)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(63, 54)
+        Button15.TabIndex = 14
+        Button15.TabStop = False
+        Button15.Text = "×"
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = SystemColors.GradientInactiveCaption
+        Button16.FlatAppearance.BorderColor = SystemColors.Control
+        Button16.FlatAppearance.BorderSize = 2
+        Button16.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button16.Location = New Point(239, 115)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(63, 54)
+        Button16.TabIndex = 15
+        Button16.TabStop = False
+        Button16.Text = "÷"
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = SystemColors.GradientInactiveCaption
+        Button17.FlatAppearance.BorderColor = SystemColors.Control
+        Button17.FlatAppearance.BorderSize = 2
+        Button17.Font = New Font("Sans Serif Collection", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button17.Location = New Point(32, 115)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(201, 54)
+        Button17.TabIndex = 16
+        Button17.TabStop = False
+        Button17.Text = "CE"
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = SystemColors.HighlightText
+        TextBox1.Font = New Font("Sans Serif Collection", 22F, FontStyle.Bold)
+        TextBox1.Location = New Point(32, 36)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.ReadOnly = True
+        TextBox1.Size = New Size(270, 73)
+        TextBox1.TabIndex = 17
+        TextBox1.TabStop = False
+        TextBox1.Text = "0"
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Button18
+        ' 
+        Button18.BackColor = Color.AliceBlue
+        Button18.FlatAppearance.BorderColor = Color.AliceBlue
+        Button18.FlatAppearance.BorderSize = 0
+        Button18.FlatAppearance.MouseOverBackColor = SystemColors.GradientInactiveCaption
+        Button18.FlatStyle = FlatStyle.Flat
+        Button18.Font = New Font("Segoe UI", 8F)
+        Button18.ForeColor = SystemColors.MenuText
+        Button18.Location = New Point(12, 7)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(75, 23)
+        Button18.TabIndex = 18
+        Button18.TabStop = False
+        Button18.Text = "Tietoja"
+        Button18.TextAlign = ContentAlignment.TopCenter
+        Button18.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.AliceBlue
+        ClientSize = New Size(334, 441)
+        Controls.Add(Button18)
+        Controls.Add(TextBox1)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MaximumSize = New Size(350, 480)
+        Name = "Form1"
+        SizeGripStyle = SizeGripStyle.Hide
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Laskin"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button18 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,165 @@
+﻿Public Class Form1
+    'Muuttujien määrittely
+    Dim ekaNum As Decimal
+    Dim tokaNum As Decimal
+    Dim operaattori As Integer
+    Dim laskutoimitus As Boolean
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Jos teksti ei ole "0", lisätään perään numero(string) "1". Jos teksti on "0", korvataan se numerolla(string) "1"
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "1"
+        Else
+            TextBox1.Text = "1"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "2"
+        Else
+            TextBox1.Text = "2"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "3"
+        Else
+            TextBox1.Text = "3"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "4"
+        Else
+            TextBox1.Text = "4"
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "5"
+        Else
+            TextBox1.Text = "5"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "6"
+        Else
+            TextBox1.Text = "6"
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "7"
+        Else
+            TextBox1.Text = "7"
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "8"
+        Else
+            TextBox1.Text = "8"
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "9"
+        Else
+            TextBox1.Text = "9"
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        'Jos teksti ei ole pelkkä numero(string) "0", lisätään "0"
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "0"
+        End If
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        If Not TextBox1.Text.Contains(",") Then ' Jos tekstissä ei ole "," lisätään se.
+            TextBox1.Text += ","
+        End If
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        'CE-näppäin. Korvataan teksti numerolla(string) "0"
+        TextBox1.Text = "0"
+        ekaNum = 0
+        tokaNum = 0
+        laskutoimitus = False
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        If laskutoimitus Then Return 'Estetään useamman operaattorin painaminen peräkkäin
+        ekaNum = Decimal.Parse(TextBox1.Text) 'Muuttujaan ekaNum tallennetaan muuttujan TextBox1 teksti(string) desimaalina(decimal)
+        operaattori = 1  'Jakolaskun operaattori
+        TextBox1.Text = "0" ' Muutetaan näytettävä teksti numero(string) "0"
+        laskutoimitus = True 'Laskutoimitus suoritetaan, kun painetaan summa painiketta
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        If laskutoimitus Then Return
+        ekaNum = Decimal.Parse(TextBox1.Text)
+        operaattori = 2  'Kertoaskun operaattori
+        TextBox1.Text = "0"
+        laskutoimitus = True
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        If laskutoimitus Then Return
+        ekaNum = Decimal.Parse(TextBox1.Text)
+        operaattori = 3  'Vähennyslaskun operaattori
+        TextBox1.Text = "0"
+        laskutoimitus = True
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        If laskutoimitus Then Return
+        ekaNum = Decimal.Parse(TextBox1.Text)
+        operaattori = 4  'Yhteenlaskun operaattori
+        TextBox1.Text = "0"
+        laskutoimitus = True
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        If laskutoimitus = True Then 'Summa-painike. Suoritetaan laskutoimitus, jos ehto on tosi tallennetaan luku toiseen muuttujaan laskemista varten
+            tokaNum = Decimal.Parse(TextBox1.Text) 'Tallennetaan luku toiseen muuttujaan laskemista varten
+            If operaattori = 1 And tokaNum <> 0 Then 'Jos operaattori on 1 ja tokaNum ei ole 0, suoritetaan jakolasku
+                TextBox1.Text = ekaNum / tokaNum ' Jakolasku
+            ElseIf operaattori = 2 And tokaNum <> 0 Then
+                TextBox1.Text = ekaNum * tokaNum ' Kertolasku
+            ElseIf operaattori = 3 And tokaNum <> 0 Then
+                TextBox1.Text = ekaNum - tokaNum 'Vähennyslasku
+            Else
+                TextBox1.Text = ekaNum + tokaNum 'Yhteenlasku
+            End If
+        End If
+
+        ekaNum = Decimal.Parse(TextBox1.Text) 'Muutetaan desimaaliksi
+        laskutoimitus = False ' Muutetaan muuttujan laskutoimitus arvo takaisin False
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        MessageBox.Show("Laskin" + Environment.NewLine +
+               "Versio: " + Application.ProductVersion + Environment.NewLine +
+               "Ohjelman tekijä: Aki Lahtinen GitHub: aki-lahtinen" + Environment.NewLine +
+               "© 2025" + Environment.NewLine + Environment.NewLine +
+               "Ohjelmaa saa käyttää ja jakaa vapaasti." + Environment.NewLine +
+               "Tekijä ei vastaa mahdollisista vahingoista." + Environment.NewLine + Environment.NewLine +
+               "Ohjelman ikoni on ladattu osoitteesta: www.flaticon.com",
+               "Tietoja",
+               MessageBoxButtons.OK,
+               MessageBoxIcon.Information)
+    End Sub
+End Class
+
